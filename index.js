566d96ae3c83744d483be85d23a1ff85a58d64ae
Benjamin Osei Adjei <benjamin.osei325@hotmail.com>
2024-06-03 14:38:59 +0100

    Add first setup files

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+console.log(process.env)
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`The app is listening on port ${port}`);
+})
